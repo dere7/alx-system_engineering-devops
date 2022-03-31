cea7c458abbe87bdd781640d1d1eb0df8497fa52
Dereje Desta <ddesta777@gmail.com>
2022-03-31 17:02:43 +0300

    Minor fix for 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
 # Setup nginx server
 exec { 'configure server':
   provider => shell,
-  command  => 'apt-get -y update;apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.html;sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://google.com; listen 80; \\n\\t}\\n" /etc/nginx/sites-available/default; service nginx restart'
+  command  => 'apt-get -y update;apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.html;sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://google.com; \\n\\t}\\n" /etc/nginx/sites-available/default; service nginx restart'
 }
